f32b26fcbedc1ae636a8d392ce15671285ba62ed
etn-43 <elivelton.developer@gmail.com>
2025-01-09 16:26:29 -0300

    Abrindo acesso ao site

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import dataLogin from "../../data/user-login.json";
 import logo from "../../images/logo.png"
 
 const Container = styled.div`
@@ -16,7 +14,7 @@ const LoginBox = styled.div`
   width: 400px;
   align-items: center;
   text-align: center;
-  margin-top: 15vh;
+  margin-top: 25vh;
 
   @media (max-width: 768px) {
     width: 80%; /* Ocupa 90% da largura da tela no mobile */
@@ -36,30 +34,6 @@ const Title = styled.img`
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-`;
-
-const Label = styled.label`
-  color: white;
-  margin-bottom: 5px;
-  text-align: center;
-`;
-
-const Input = styled.input`
-  width: 90%;
-  height: 30px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  margin-bottom: 15px;
-  margin-left: 3%;
-  color: #fff;
-  background-color: #050a30;
-  border: 1px solid #3E5066;
-  border-radius: 15px;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    font-size: 14px;
-  }
 `;
 
 const Button = styled.button`
@@ -89,17 +63,10 @@ const Button = styled.button`
 
 
 function Login () {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
     const handleLogin = (event) => {
-        if (dataLogin[0].email === email && dataLogin[0].password === password){
-            localStorage.setItem('token', JSON.stringify({token: "A1bC3dE5fG7hI9jK0lM"}));
-            navigate("/home");
-        } else {
-            alert("Email ou senha inválidos!");
-        }
+        navigate("/home");
         event.preventDefault();
     }
     return (
@@ -107,22 +74,7 @@ function Login () {
         <LoginBox>
         <Title src={logo} alt="logo"/>
             <Form onSubmit={handleLogin}>
-                <Label for="email">E-mail:</Label>
-                <Input 
-                    type="email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-                    placeholder="Insira seu e-mail"
-                />
-                <Label for="password">Senha:</Label>
-                <Input 
-                    type="password" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} 
-                    placeholder="Insira sua senha"
-                />
-                <br/>
-                <Button type="submit">Login</Button>
+                <Button type="submit">Iniciar</Button>
             </Form>
       </LoginBox>
     </Container>
